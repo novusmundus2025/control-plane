6d7c27bf271147285025ba48688f8d28860d3c3f
novusmundus2025 <novusmundus2025@gmail.com>
2026-09-10 22:55:59 +0200

    Ship compact table helper to Chat browser

--- apps/chat/src/main.js
+++ apps/chat/src/main.js
@@ -5025,6 +5025,7 @@ button,input,select,textarea { font-family:inherit; } code,pre,kbd,samp { font-f
       flushParagraph();
     }
 
+    ${expandCompactMarkdownTable.toString()}
     ${normalizeAssistantDisplayText.toString()}
     ${protectMathSegments.toString()}
 
